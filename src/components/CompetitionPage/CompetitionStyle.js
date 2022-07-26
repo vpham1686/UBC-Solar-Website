91b7c05a4db92032140108d877c5d32c44546f00
Mark De Guzman <markdeguzman1142@gmail.com>
2022-07-25 23:58:27 -0700

    created Competitions page layout including navbar

--- src/components/CompetitionPage/CompetitionStyle.js
+++ src/components/CompetitionPage/CompetitionStyle.js
@@ -1,6 +1,18 @@
 import styled from 'styled-components';
 
-export const CompetitionContainer = styled.div`
-    background: #555555;
-    height: 80px;
+export const CompetitionImageContainer = styled.div`
+    height: 100%;
+    width: 100%;
+`;
+
+export const CompetitionImage = styled.img`
+    height: 90%;
+    width: 100%;
+    object-fit: cover;
+    display: block;
+`;
+
+export const CompetitionTextContainer = styled.div`
+    text-align: center;
+    padding: 10% 20%;
 `;
--- src/components/CompetitionPage/index.js
+++ src/components/CompetitionPage/index.js
@@ -1,15 +1,59 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import NavigationBar from '../../components/NavigationBar';
+import Sidebar from '../../components/Sidebar';
 import {
-    CompetitionContainer,
-
+    CompetitionImageContainer,
+    CompetitionImage,
+    CompetitionTextContainer,
 } from './CompetitionStyle';
 
 const CompetitionPage = () => {
-    return (
-        <CompetitionContainer>
+    const [isOpen, setIsOpen] = useState(false);
 
-        </CompetitionContainer>
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <>
+            <NavigationBar isOpen={isOpen }toggle={toggle} />
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <div>
+                <CompetitionImageContainer>
+                    <CompetitionImage src={'https://dummyimage.com/1920x1080/002145/ffffff&text=Placeholder+Video'} alt="Main Image" />
+                </CompetitionImageContainer>
+                <CompetitionImageContainer>
+                    <CompetitionImage src={'https://dummyimage.com/1920x1080/002145/ffffff&text=First+Competition+Image'} alt="Competition Image 1" />
+                </CompetitionImageContainer>
+                <CompetitionTextContainer>
+                    <h1 style={{marginBottom: '30px'}}>Formula Sun Grand Prix 2017</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem 
+                        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, 
+                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </CompetitionTextContainer>
+                <CompetitionImageContainer>
+                    <CompetitionImage src={'https://dummyimage.com/1920x1080/002145/ffffff&text=Second+Competition+Image'} alt="Competition Image 2" />
+                </CompetitionImageContainer>
+                <CompetitionTextContainer>
+                    <h1 style={{marginBottom: '30px'}}>Formula Sun Grand Prix 2022</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem 
+                        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, 
+                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </CompetitionTextContainer>
+            </div>
+        </>
     );
 };
 
