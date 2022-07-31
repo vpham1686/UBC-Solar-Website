c2e2cd67a3f6dd6ea6eaddb62044470dbef4f91b
vpham1686 <victordkpham888@gmail.com>
2022-07-31 15:00:37 -0700

    Primper for teamrole page, just need styling/hover

--- src/components/TeamRolePage/index.js
+++ src/components/TeamRolePage/index.js
@@ -1,11 +1,25 @@
 import React, { useState } from 'react';
 import NavigationBar from '../../components/NavigationBar';
 import Sidebar from '../../components/Sidebar';
+import { FaAngleDown } from 'react-icons/fa';
+
+import TeamRoleHero from '../../assets/TeamRoleImages/TeamRoles.png';
 
 import Footer from '../Footer';
 
+//Team Leads
+
 import {
     TeamRoleContainer,
+    HeroContainer,
+    HeroPhoto,
+    ArrowLevitate,
+    TeamLeadContainer,
+    LeadProfileContainer,
+    LeadImage,
+    LeadDescription,
+    LeadName,
+    LeadPosition,
 
 } from './TeamRoleStyle';
 
@@ -20,8 +34,25 @@ const TeamRolePage = () => {
         <>
         <NavigationBar isOpen={isOpen }toggle={toggle} />
         <Sidebar isOpen={isOpen} toggle={toggle} />
+
             <TeamRoleContainer>
-                Team Roles.
+                <HeroContainer>
+                        <HeroPhoto
+                            src={ TeamRoleHero }>
+                        </HeroPhoto>
+                        <ArrowLevitate to='meet'>
+                        <FaAngleDown size="80px" cursor="pointer" ></FaAngleDown>
+                        </ArrowLevitate>
+                    </HeroContainer>
+
+                <TeamLeadContainer>
+                    <LeadProfileContainer>
+                        <LeadImage></LeadImage>
+                        <LeadDescription></LeadDescription> //Hover Effect
+                        <LeadName>Daddy</LeadName>
+                        <LeadPosition>Your Mom</LeadPosition>
+                    </LeadProfileContainer>
+                </TeamLeadContainer>
             </TeamRoleContainer>
 
             <Footer/>
