7ae332fcca5ef6efcf961824525e43db0686a924
vpham1686 <victordkpham888@gmail.com>
2022-08-21 22:18:07 -0700

    Updated

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Squash as Hamburger } from 'hamburger-react'
 import Logo from '../../assets/NavigationBarImages/Logo.svg';
 import {
