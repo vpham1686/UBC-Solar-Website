e5fd8681d78311bcebae0d1e989c4c141fdca0de
vpham1686 <victordkpham888@gmail.com>
2022-08-23 18:28:13 -0700

    UpdatedFinal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages';
 import ProjectPage from "./components/ProjectPage";
+import CompetitionPage from "./components/CompetitionPage";
+import TeamRolePage from "./components/TeamRolePage";
+import RecruitmentPage from "./components/JoinUsPage";
 
 function App() {
 
@@ -26,6 +29,9 @@ function App() {
       {contacts()}
       <Route path="/" element={<Home />} />
       <Route path="/projects" element={<ProjectPage />} />
+      <Route path="/competitions" element={<CompetitionPage />} />
+      <Route path="/team-roles" element={<TeamRolePage />} />
+      <Route path="/recruitment" element={<RecruitmentPage />} />
       {/* Should create an error page (Something simple like an image or something) for any invalid routes */}
       <Route path="*" element={<Home />} />
     </Routes>
