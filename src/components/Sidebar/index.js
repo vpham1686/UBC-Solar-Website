4faec60498775a183328acf2e3eda86579b7d20f
vpham1686 <victordkpham888@gmail.com>
2022-08-24 17:58:43 -0700

    Clean

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import {
     SidebarContainer,
-    CloseIcon,
-    Icon,
     SidebarWrapper,
     SidebarMenu,
-    SidebarLink,
     SideBtnWrap,
     SidebarRoute
 } from './SidebarStyle';
