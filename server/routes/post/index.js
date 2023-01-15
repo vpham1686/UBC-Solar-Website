b61e1702acfc3ad6d1ac9de577c05232d03271bb
Mark De Guzman <markdeguzman1142@gmail.com>
2023-01-14 21:10:27 -0800

    Added proper error management and updated UI.

--- server/routes/post/index.js
+++ server/routes/post/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const router = express.Router()
 const profile = require('../../model/profile');
 const asyncWrapper = require('../../common/asyncWrapper');
+const { ProfileBadRequestError, ProfileDbError, ProfileMissingIdError, ProfileNotFoundError, InvalidRouteError } = require('../../errors/errorHandling');
 
 router.post("/profile", asyncWrapper(async (req, res) => {
     const user = { name, email, bio, dateJoined, linkedin, role, socialMedia } = req.body;
@@ -10,9 +11,9 @@ router.post("/profile", asyncWrapper(async (req, res) => {
         res.status(400).json({ "errorMsg": "User not found" });
     }
     await profile.create(user).catch(err => {
-        res.status(400).json({ "errorMsg": err });
+        throw new ProfileDbError("Error creating user: Please check documentation");
     })
-    res.send("Data has been posted");
+    res.send("User created");
 }));
 
 module.exports = router;
