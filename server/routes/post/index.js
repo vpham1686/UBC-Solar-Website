8a22df3de7d25841368e4f23a321de6530b13cdc
mark-p7 <markdeguzman1142@gmail.com>
2023-01-14 19:44:16 -0800

    Created routes folder and added POST route.

--- server/routes/post/index.js
+++ server/routes/post/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const router = express.Router()
-router.post("/profile", (req, res) => {
-    const user = {name, surname, subteam, dateJoined, blurb, linkedin, academicInfo, role, email} = req.body;
+const profile = require('../../model/profile');
+const asyncWrapper = require('../../common/asyncWrapper');
+
+router.post("/profile", asyncWrapper(async (req, res) => {
+    const user = { name, email, bio, dateJoined, linkedin, role, socialMedia } = req.body;
     if (!user) {
-        res.status(400).json({"errorMsg": "User not found"})
+        res.status(400).json({ "errorMsg": "User not found" });
     }
-    res.send("Data has been posted")
-})
+    await profile.create(user).catch(err => {
+        res.status(400).json({ "errorMsg": err });
+    })
+    res.send("Data has been posted");
+}));
+
 module.exports = router;
